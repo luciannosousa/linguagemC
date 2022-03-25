c40825cbbcefc9ea3d7d51efbfefb0ce2fd8d1d8
luciannosousa <luciannofs@gmail.com>
2022-03-25 10:04:37 -0300

    soma de valores

---
+++ exercicicos/soma.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdio.h>
+void main() {
+    int soma, num1, num2;
+    printf("informe o primeiro numero:");
+    scanf("%d", &num1);
+    printf("informe)o segundo numero:");
+    scanf("%d",&num2);
+
+    soma= num1 + num2;
+    printf("resultado da soma: %d", soma);
+    
+}
